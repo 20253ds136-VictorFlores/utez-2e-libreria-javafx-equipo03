ade94863ab2881b1ddb66a3c7f7fef14acbf5696
Alejandro Salgado <20253ds149@utez.edu.mx>
2026-03-22 00:32:18 -0600

    Creacion del proyecto JavaFX: IntegradoraBiblioteca

---
+++ IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/HelloController.java
@@ -0,0 +1,14 @@
+package com.resources.integradorabiblioteca;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
