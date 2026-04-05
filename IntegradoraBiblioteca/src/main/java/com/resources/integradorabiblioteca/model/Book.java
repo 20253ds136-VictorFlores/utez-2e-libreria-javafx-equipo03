76caff920551aeb1551885300a15eb87787fa76a
Alejandro Salgado <20253ds149@utez.edu.mx>
2026-04-05 15:21:18 -0600

    Actualizacion en el archivo book (se añadieron los atributos)

---
+++ IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/model/Book.java
@@ -0,0 +1,14 @@
+package com.resources.integradorabiblioteca.model;
+
+/**
+ * Modelo que representa un libro en el catálogo.
+ */
+public class Book {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int anio;
+    private String genero;
+    private boolean disponible;
+
+}
