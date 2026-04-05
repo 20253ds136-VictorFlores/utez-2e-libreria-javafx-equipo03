9ec0a93022c802b283a2e1b384416a8ff4832c69
Alejandro Salgado <20253ds149@utez.edu.mx>
2026-04-05 13:42:33 -0600

    Creacion de carpeta model y archivo book, actualizacion en el archivo RunApplication

--- IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/RunApplication.java
+++ IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/RunApplication.java
@@ -4,16 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class RunApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RunApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(RunApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 850, 600);
+        stage.setTitle("Sistema de Gestión - Biblioteca Escolar");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
