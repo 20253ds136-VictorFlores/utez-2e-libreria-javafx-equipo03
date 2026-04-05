5f215e4e7b9267d48a4899dd2ba0c777f373a099
Alejandro Salgado <20253ds149@utez.edu.mx>
2026-04-05 15:26:45 -0600

    Actualizacion en el archivo book (se añadio el constructor)

--- IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/model/Book.java
+++ IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/model/Book.java
@@ -11,4 +11,21 @@ public class Book {
     private String genero;
     private boolean disponible;
 
+    /**
+     * Inicializa un libro con los datos dados.
+     * @param isbn código ISBN
+     * @param titulo título del libro
+     * @param autor autor del libro
+     * @param anio año de publicación
+     * @param genero género literario
+     * @param disponible disponibilidad en el catálogo
+     */
+    public Book(String isbn, String titulo, String autor, int anio, String genero, boolean disponible) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.genero = genero;
+        this.disponible = disponible;
+    }
 }
