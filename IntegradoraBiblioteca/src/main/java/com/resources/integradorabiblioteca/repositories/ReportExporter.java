970d84d4e6528e53bb0329d0331b869f46b398f1
Alejandro Salgado <20253ds149@utez.edu.mx>
2026-04-05 17:06:51 -0600

    Actualizacion en el archivo ReportExporter (se creo el metodo exportarCatalogo)

--- IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/repositories/ReportExporter.java
+++ IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/repositories/ReportExporter.java
@@ -1,4 +1,31 @@
 package com.resources.integradorabiblioteca.repositories;
 
+import com.resources.integradorabiblioteca.model.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Exporta el catálogo de libros a un archivo CSV.
+ */
 public class ReportExporter {
+
+    /**
+     * Genera un archivo CSV con los datos del catálogo.
+     * @param listaLibros colección de libros a exportar
+     * @throws IOException si ocurre un error de escritura
+     */
+    public void exportarCatalogo(List<Book> listaLibros) throws IOException {
+        BufferedWriter escritor = new BufferedWriter(new FileWriter("reporte_catalogo.csv"));
+
+        escritor.write("ISBN,Titulo,Autor,Anio,Genero,Disponible");
+        escritor.newLine();
+
+        for (Book libro : listaLibros) {
+            escritor.write(libro.toString());
+            escritor.newLine();
+        }
+        escritor.close();
+    }
 }
