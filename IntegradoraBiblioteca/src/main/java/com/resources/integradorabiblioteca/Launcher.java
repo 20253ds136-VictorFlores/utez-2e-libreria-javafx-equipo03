59d3a966249d7f18223f733ad5dad0878f264b00
Alejandro Salgado <20253ds149@utez.edu.mx>
2026-04-05 13:47:09 -0600

    Actualizacion en el archivo Launcher

--- IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/Launcher.java
+++ IntegradoraBiblioteca/src/main/java/com/resources/integradorabiblioteca/Launcher.java
@@ -1,9 +1,7 @@
 package com.resources.integradorabiblioteca;
 
-import javafx.application.Application;
-
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(RunApplication.class, args);
+        RunApplication.main(args);
     }
 }
